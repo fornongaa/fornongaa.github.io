bf8fb5037a336fc31770d272b1544314a95e5537
yaikaew <yaikaew46@gmail.com>
2024-05-31 00:01:42 +0700

    new ui

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-function toggleMenu() {
-    var menu = document.getElementById("menu");
-    if (menu.style.display === "block") {
-      menu.style.display = "none";
-    } else {
-      menu.style.display = "block";
-    }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  
+}
