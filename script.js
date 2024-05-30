16e5980bd8b6f2deec271a9281c889253ebfe325
yaikaew <yaikaew46@gmail.com>
2024-05-30 17:18:08 +0700

    new ui

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function() {
-    navbar = document.querySelector(".nav-bar");
-    navbar.classList.toggle("active")
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.getElementById('menu');
+
+    menuToggle.addEventListener('click', function() {
+        menu.classList.toggle('show');
+    });
+
+    // Add this to remove the 'show' class when window is resized to larger screens
+    window.addEventListener('resize', function() {
+        if (window.innerWidth > 768) {
+            menu.classList.remove('show');
+        }
+    });
+});
